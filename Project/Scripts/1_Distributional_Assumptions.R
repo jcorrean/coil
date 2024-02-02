1a42d5fd1385b661931303961d48f1c544960aaf
Juan C. Correa <j.correa.n@gmail.com>
2024-02-01 18:44:12 -0600

    Create 1_Distributional_Assumptions.R

---
+++ Project/Scripts/1_Distributional_Assumptions.R
@@ -0,0 +1,16 @@
+library(readr)
+coildata <- coildata <- read_csv("Data/coildata.csv")
+
+library(ggplot2)
+coildata_long <- reshape2::melt(coildata)
+
+png("Results/F2.png", width = 15, height = 8, units = 'in', res = 300)
+ggplot(coildata_long, aes(x = value, fill = variable)) +
+  geom_density(alpha = 0.5) +
+  facet_wrap(~variable, scales = "free", ncol = 5) +
+  theme_minimal() +  # Optional: Customize the theme
+  labs(title = "Statistical distributions of observed variables")
+dev.off()
+
+library(MVN)
+mvn(coildata)

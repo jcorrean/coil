fa210ae12b3af12d50dff0f9a69896d462c41e8c
Juan C. Correa <j.correa.n@gmail.com>
2024-02-04 05:35:41 -0600

    Update Unidimensional_Coordination.R

--- Project/Scripts/Unidimensional_Coordination.R
+++ Project/Scripts/Unidimensional_Coordination.R
@@ -32,3 +32,47 @@ CD <- summary(result2, fit.measures=TRUE)
 CD$test
 CD <- data.frame(CD$fit)
 CD$Index <- rownames(CD)
+
+
+library(lavaan);
+modelData <- coildata
+modelCD<-"
+! regressions 
+   CD=~CD__CD1*CD1
+   CD=~CD__CD2*CD2
+   CD=~CD__CD4*CD4
+! residuals, variances and covariances
+   CD1 ~~ VAR_CD1*CD1
+   CD2 ~~ VAR_CD2*CD2
+   CD4 ~~ VAR_CD4*CD4
+   CD ~~ 1.0*CD
+! observed means
+   CD1~1;
+   CD2~1;
+   CD4~1;
+";
+
+
+
+result6 <- cfa(modelCD, data=coildata, fixed.x=FALSE, estimator="MLM", std.ov = TRUE, std.lv = TRUE);
+
+CD2 <- summary(result6, fit.measures=TRUE)
+CD2 <- data.frame(CD2$fit)
+CD2
+
+library(semPlot)
+png("Results/F4C.png", width = 15, height = 10, units = 'in', res = 300)
+semPaths(result6, 
+         whatLabels = "std", 
+         layout = "tree", 
+         color = list(lat = rgb(255, 0, 0, maxColorValue = 255),
+                      man = rgb(255, 102, 102, maxColorValue = 255)),
+         edge.color = "black",
+         shapeMan = "rectangle",
+         edge.label.cex = 1.5,
+         edge.width = 1.5,
+         label.cex = 1.5,
+         node.width = 2,
+         node.height = 2,
+         mar = c(6, 1.2, 9, 1.2), title = FALSE, intercepts = FALSE, residuals = TRUE, nCharNodes = 0)
+dev.off()

353bb516de9b8ff57c5754e3ce7cf1c43adb1db9
Juan C. Correa <j.correa.n@gmail.com>
2024-02-04 22:18:19 -0600

    Create Estimated_Parameters_Analysis.R

---
+++ Project/Scripts/Estimated_Parameters_Analysis.R
@@ -0,0 +1,3 @@
+load("~/Documents/GitHub/coil/Project/Results/lslx_Estimates.RData")
+load("~/Documents/GitHub/coil/Project/Results/MIIV_Estimates.RData")
+load("~/Documents/GitHub/coil/Project/Results/ML_Estimates.RData")

e13b6afbdff540a57de4e6d3d1d940a1cde3ef79
Juan C. Correa <j.correa.n@gmail.com>
2024-02-06 06:53:27 -0600

    Update 1_Distributional_Assumptions.R

--- Project/Scripts/1_Distributional_Assumptions.R
+++ Project/Scripts/1_Distributional_Assumptions.R
@@ -8,8 +8,9 @@ png("Results/F2.png", width = 15, height = 8, units = 'in', res = 300)
 ggplot(coildata_long, aes(x = value, fill = variable)) +
   geom_density(alpha = 0.5) +
   facet_wrap(~variable, scales = "free", ncol = 5) +
-  theme_minimal() +  # Optional: Customize the theme
-  labs(title = "Statistical distributions of observed variables")
+  theme_minimal() + # Optional: Customize the theme
+  theme(legend.position = "none") + # This line removes the legend
+  labs(title = "")
 dev.off()
 
 library(MVN)
